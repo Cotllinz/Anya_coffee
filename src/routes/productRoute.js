1e9c46fa24f1756ef18b6a432f8252171760c1ba
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2020-12-10 21:49:41 +0800

    Update productRoute.js

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,6 +1,11 @@
 const route = require('express').Router()
-const { getProduct, AddProduct } = require('../controller/productController')
+const {
+  getProduct,
+  AddProduct,
+  getProductById
+} = require('../controller/productController')
 
 route.get('/', getProduct)
 route.post('/', AddProduct)
+route.get('/:id', getProductById)
 module.exports = route
