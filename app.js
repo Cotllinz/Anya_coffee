9ccc6dcd1db9b253099bb54145d8af52dd2372a7
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2020-12-13 22:02:44 +0800

    Update Cors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const smash = require('morgan')
 const bodyParser = require('body-parser')
 const routeNavigation = require('./src/routeNavigation')
-
+const cors = require('cors')
 const app = express()
 app.use(smash('dev'))
 
@@ -10,7 +10,15 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/', routeNavigation)
-
+app.use(cors())
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*')
+  response.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Request-With, Content-Type, Accept, Authorization'
+  )
+  next()
+})
 app.get('*', (req, res) => {
   res.status(404).send('Not Found Please Check again ! :)')
 })
