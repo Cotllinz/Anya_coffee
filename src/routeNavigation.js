c891ec8f0c72bb7e6af3a3d808d4e07b65c8de93
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2020-12-13 22:03:22 +0800

    Add Category And History

--- src/routeNavigation.js
+++ src/routeNavigation.js
@@ -1,8 +1,12 @@
 const routeing = require('express').Router()
 const product = require('./routes/productRoute')
 const promo = require('./routes/promoRoute')
+const category = require('./routes/categoryRoute')
+const history = require('./routes/historyRoute')
 
 routeing.use('/product', product)
 routeing.use('/promo', promo)
+routeing.use('/category', category)
+routeing.use('/history', history)
 
 module.exports = routeing
