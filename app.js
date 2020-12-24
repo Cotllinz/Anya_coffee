720f582a6a7eef3088d092163a8bc50ab3aa9064
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2020-12-25 03:57:42 +0800

    Update Edit Image, Add Image & Delete Image to Database Using FS unlink and All Condition Requirement

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const cors = require('cors')
 require('dotenv').config()
 const app = express()
 app.use(smash('dev'))
+app.use(express.static('userImage'))
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 const port = process.env.PORT
 app.use('/', routeNavigation)
-
 app.use((request, response, next) => {
   response.header('Access-Control-Allow-Origin', '*')
   response.header(
