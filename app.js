866790efcdd0c2ea50a858bd8a85c0c9c7ad5870
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2021-02-17 00:37:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ const cors = require('cors')
 require('dotenv').config()
 const app = express()
 app.use(smash('dev'))
+app.use('/api1/fileuplodanya', express.static('userImage'))
+app.use('/api1/fileuplodanya', express.static('productImage'))
 app.use(express.static('userImage'))
 app.use(express.static('productImage'))
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 const port = process.env.PORT
-app.use('/', routeNavigation)
+app.use('/api1', routeNavigation)
 app.use((request, response, next) => {
   response.header('Access-Control-Allow-Origin', '*')
   response.header(
